7f1093653aebaa3a390a66f647cd64c41daf0bc9
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-06-04 00:03:44 -0500

    get list works. Database created

--- public/scripts/toDo.js
+++ public/scripts/toDo.js
@@ -1,3 +1,35 @@
+//on page load
 $(document).ready(function() {
   console.log('JQJS');
+  getList();
+  //submit button click function
+  $('.submit').on('click', addToList);
 });
+
+//add to list funciton
+function addToList() {
+  console.log('button clicked');
+  var requestObject = {
+    task: $('.task').val(),
+    notes: $('.notes').val()
+  };
+  console.log(requestObject);
+} //end add to list
+
+//get list function
+function getList() {
+  $.ajax({
+    type: 'GET',
+    url: '/getList',
+    success: function(response) {
+      console.log(response);
+      for (var i = 0; i < response.length; i++) {
+        var $div = $('<div>');
+        $div.addClass('taskItem');
+        $div.append('<h3>Task:</h3>' + response[i].task);
+        $div.append('<h3>Notes:</h3>' + response[i].notes);
+        $('.display').append($div);
+      }
+    }
+  });
+} //end get list
--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(bodyParser.urlencoded({
 
 //globals
 var port = 7575;
+var config = {
+  database: 'omega',
+  host: 'localhost',
+  port: 5432,
+  max: 20
+};
+var pool = new pg.Pool(config);
 
 //listen
 app.listen(port, function() {
@@ -23,3 +30,24 @@ app.listen(port, function() {
 app.get('/', function(req, res) {
   res.sendFile(path.resolve('views/index.html'));
 });
+
+//get list
+app.get('/getList', function(req, res) {
+  pool.connect(function(err, connection, done) {
+    if (err) {
+      console.log('error');
+      done();
+      res.sendStatus(400);
+    } else {
+      var tasks = [];
+      var results = connection.query('SELECT * FROM todo');
+      results.on('row', function(row) {
+        tasks.push(row);
+      });
+      results.on('end', function() {
+        done();
+        res.send(tasks);
+      });
+    }
+  }); //end pool.connect
+}); //end get list
